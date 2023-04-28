4e26ec4d44158c834181ae0132b50c97f7c9bc3a
Shamil <shamval@mail.ru>
2023-04-28 12:35:21 +0300

    Began working on app (finished)

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+# Мои приложения
+    'learning_logs',
+    # Приложения django по умолчанию
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
