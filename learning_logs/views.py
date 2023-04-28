b270e6099c0797b6c8173a160a5bb1f02d14cd2a
Shamil <shamval@mail.ru>
2023-04-28 18:47:14 +0300

    Finished chapter 18.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -7,7 +7,14 @@ def index(request):
 	return render(request, 'learning_logs/index.html')
 
 def topics(request):
-	# Выводит список тем
+	# Выводит список тем - НУЖНО ЛИ??? УДАЛИТЬ???
 	topics = Topic.objects.order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+	# Выводит одну тему и все её записи
+	topic = Topic.objects.get(id=topic_id)
+	entries = topic.entry_set.order_by('-date_added')
+	context = {'topic': topic, 'entries': entries}
+	return render(request, 'learning_logs/topic.html', context)
