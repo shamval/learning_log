d83e7122391901b594c315e0b87ab0af0efa2e96
Shamil <shamval@mail.ru>
2023-04-28 17:24:06 +0300

    Made Topics template.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
 	# Домашняя страница приложения LearningLog
 	return render(request, 'learning_logs/index.html')
+
+def topics(request):
+	# Выводит список тем
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'learning_logs/topics.html', context)
