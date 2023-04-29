d2d026cbf2d71b1018f8b13bfdb6114e5c9865a9
Shamil <shamval@mail.ru>
2023-04-29 15:21:27 +0300

    You can now create a new entry, yet without possibility of changing it.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-	#Домашняя страница
+	# Домашняя страница
 	path('', views.index, name='index'),
 	# Страница со списком всех тем
 	path('topics/', views.topics, name='topics'),
@@ -13,4 +13,6 @@ urlpatterns = [
 	path('topics/<int:topic_id>/', views.topic, name='topic'),
 	# Страница для добавления новой темы
 	path('new_topic/', views.new_topic, name='new_topic'),
+	# Страница для добавления новой записи
+	path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
