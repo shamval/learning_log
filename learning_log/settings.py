dfee1722e00572fa946f619b2ce19e8e975461f7
Shamil <shamval@mail.ru>
2023-05-01 18:45:10 +0300

    Styled the Home Page using a jumbotron, page 461

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
 # Мои приложения
     'learning_logs',
     'users',
+
+    # Сторонние приложения
+    'bootstrap4',
+    
     # Приложения django по умолчанию
     'django.contrib.admin',
     'django.contrib.auth',
